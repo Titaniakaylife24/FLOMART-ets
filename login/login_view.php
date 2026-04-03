1ef514d9227864de5f84669f6cca14dd2a643b39
Titaniakaylife24 <titakaylife@gmail.com>
2026-04-03 21:56:39 +0700

    Memperbaiki code

--- login/login_view.php
+++ login/login_view.php
@@ -11,7 +11,7 @@
 <form action="proses_login.php" method="POST">
     <input type="email" name="email" placeholder="Email"><br>
     <input type="password" name="password" placeholder="Password"><br>
-    <button type="submit">Login</button>
+    <button type="submit" name="login">Login</button>
 </form>
 
 </body>
